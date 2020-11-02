6825e65107fdadc46bcd0201bd94e1f11a935171
Michał <michal.czech00@gmail.com>
2020-11-02 02:09:06 +0100

    Rename file

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React from "react"
 import banner from '../images/banner.jpg'
 
-import Banner from '../components/Banner'
+import Banner from './Banner'
 class Header extends React.Component {
   render () {
     return (
